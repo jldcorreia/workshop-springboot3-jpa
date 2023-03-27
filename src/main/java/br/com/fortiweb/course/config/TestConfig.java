c69012716638204184245d28c07b18815738a7e1
Jassa <jacinto.leonardo@gmail.com>
2023-03-26 21:44:59 -0300

    Order Item, many-to-many association with extra attributes part one

--- src/main/java/br/com/fortiweb/course/config/TestConfig.java
+++ src/main/java/br/com/fortiweb/course/config/TestConfig.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import br.com.fortiweb.course.entities.Category;
 import br.com.fortiweb.course.entities.Order;
@@ -18,7 +19,7 @@ import br.com.fortiweb.course.repositories.ProductRepository;
 import br.com.fortiweb.course.repositories.UserRepository;
 
 @Configuration
-//@Profile("test")
+@Profile("test")
 public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
@@ -48,6 +49,18 @@ public class TestConfig implements CommandLineRunner {
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 		
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		
+		
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
--- src/main/java/br/com/fortiweb/course/entities/Product.java
+++ src/main/java/br/com/fortiweb/course/entities/Product.java
@@ -6,11 +6,14 @@ import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
@@ -26,7 +29,11 @@ public class Product implements Serializable {
 	private Double price;
 	private String imgUrl;
 	
-	@Transient
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_product_category", 
+	           joinColumns = @JoinColumn(name = "product_id"),
+	           inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 	
 	
