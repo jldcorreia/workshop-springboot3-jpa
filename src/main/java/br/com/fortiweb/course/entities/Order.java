3c2d66b5999f073e57224501ad397becebe33708
Jassa <jacinto.leonardo@gmail.com>
2023-03-27 08:53:59 -0300

    Subtotal & Total methods

--- src/main/java/br/com/fortiweb/course/entities/Order.java
+++ src/main/java/br/com/fortiweb/course/entities/Order.java
@@ -102,6 +102,10 @@ public class Order implements Serializable {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+	
+	public Double getTotal() {
+		return items.stream().mapToDouble(x -> x.getSubTotal()).sum();
+	}
 
 	@Override
 	public int hashCode() {
